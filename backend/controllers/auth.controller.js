5789be8d7c47d6b1474e346144a3de3cab0e445b
manikerisaurabh <manikerisaurabh@gmail.com>
2024-03-30 02:36:10 +0530

    implemented logout route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -84,6 +84,11 @@ export const login = async (req, res) => {
 
 
 export const logout = (req, res) => {
-    console.log("logout");
-    res.send("logout");
+    try {
+        res.cookie("jwt", "", { maxAge: 0 });
+        res.status(200).json({ message: "Logged out successfully" });
+    } catch (error) {
+        console.log("Error in login controller : " + error.message);
+        return res.status(500).json({ error: "Internal server error" });
+    }
 };
