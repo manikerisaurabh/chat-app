95cceee366db90328718cf258e1a6399fabb17ae
manikerisaurabh <manikerisaurabh@gmail.com>
2024-03-31 19:18:28 +0530

    designed sidebar

---
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SearchInput from './SearchInput';
+const Sidebar = () => {
+    return (
+        <div>
+            {/* search input */}
+            <SearchInput />
+            <div className='divider px-3'></div>
+            {/* COnvertiation list */}
+            {/* logout button */}
+        </div>
+    )
+}
+
+export default Sidebar
