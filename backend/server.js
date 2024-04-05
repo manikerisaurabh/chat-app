b2d24d912ee09650d75c5e3517561dddca9346ae
manikerisaurabh <manikerisaurabh@gmail.com>
2024-04-06 00:12:08 +0530

    did setup for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser';
@@ -9,12 +10,20 @@ import { app, server } from './socket/socket.js'
 dotenv.config();
 const PORT = 8080;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoute);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 
 app.get("/", (req, res) => {
     res.send("root route");
